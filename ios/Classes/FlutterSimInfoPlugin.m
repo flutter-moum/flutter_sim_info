2a81f956d88faf64d9af94efaf17b272f7a7365b
kwangwoo <rhkddn1657@gmail.com>
2019-10-01 19:35:18 +0900

    Deleted

--- ios/Classes/FlutterSimInfoPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FlutterSimInfoPlugin.h"
-#import <flutter_sim_info/flutter_sim_info-Swift.h>
-
-@implementation FlutterSimInfoPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterSimInfoPlugin registerWithRegistrar:registrar];
-}
-@end
