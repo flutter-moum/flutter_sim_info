e954b702c3545c232c705bdaf1326783eb5c5b54
eungikimme <eungikim.me@gmail.com>
2019-10-06 04:09:59 +0900

    feat: add telephony manager
    And test useage

--- android/src/main/java/flutter/moum/sim_info/SimInfoPlugin.java
+++ android/src/main/java/flutter/moum/sim_info/SimInfoPlugin.java
@@ -1,5 +1,16 @@
 package flutter.moum.sim_info;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.telephony.TelephonyManager;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -8,18 +19,77 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** SimInfoPlugin */
 public class SimInfoPlugin implements MethodCallHandler {
+  private final Activity mActivity;
+  private final TelephonyManager mTelephonyManager;
+
+  private SimInfoPlugin(Activity activity) {
+    mActivity = activity;
+    mTelephonyManager = (TelephonyManager) activity.getSystemService(Context.TELEPHONY_SERVICE);
+  }
+
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "sim_info");
-    channel.setMethodCallHandler(new SimInfoPlugin());
+
+    channel.setMethodCallHandler(new SimInfoPlugin(registrar.activity()));
   }
 
   @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    if (hasPermission(Manifest.permission.READ_PHONE_STATE)) {
+      result.error("Permission Denied", null, null);
+      return;
     }
+
+    switch (call.method) {
+      case "allowsVOIP":
+        result.success(true);
+        break;
+      case "carrierName":
+        getCarrierName(result);
+        break;
+      case "isoCountryCode":
+        break;
+      case "mobileCountryCode":
+        break;
+      case "mobileNetworkCode":
+        break;
+      case "getPlatformVersion":
+        getPlatformVersion(result);
+        break;
+      default:
+        result.notImplemented();
+    }
+  }
+
+  @SuppressWarnings("SameParameterValue")
+  private boolean hasPermission(String permission) {
+    return PackageManager.PERMISSION_DENIED ==
+            ContextCompat.checkSelfPermission(mActivity, permission);
+  }
+
+  private void getCarrierName(@NonNull Result result) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+      String carrierName = mTelephonyManager.getSimCarrierIdName().toString();
+      result.success(carrierName);
+    }
+  }
+  private String getIsoCountryCode() {
+    return "";
+  }
+  private String getMobileCountryCode() {
+    return "";
+  }
+  private String getMobileNetworkCode() {
+    return "";
+  }
+
+  private boolean isSimStateReady() {
+    return true;
+  }
+
+
+  private void getPlatformVersion(Result result) {
+    result.success("Android " + android.os.Build.VERSION.RELEASE);
   }
 }
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
+  String _carrierIdName = 'Unknown';
 
   @override
   void initState() {
@@ -23,11 +24,14 @@ class _MyAppState extends State<MyApp> {
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     String platformVersion;
+    String carrierIdName;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       platformVersion = await SimInfo.platformVersion;
+      carrierIdName = await SimInfo.carrierName;
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
+      carrierIdName = "carrierIdName";
     }
 
     // If the widget was removed from the tree while the asynchronous platform
@@ -37,6 +41,7 @@ class _MyAppState extends State<MyApp> {
 
     setState(() {
       _platformVersion = platformVersion;
+      _carrierIdName = carrierIdName;
     });
   }
 
@@ -47,8 +52,15 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Column(
+          children: <Widget>[
+            Center(
+              child: Text('Running on: $_platformVersion\n'),
+            ),
+            Center(
+              child: Text('Carrier Name: $_carrierIdName\n'),
+            ),
+          ],
         ),
       ),
     );
