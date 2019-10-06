3a359f61ec9c3af1a45571891bd7dfb25f45001a
kwangwoo <rhkddn1657@gmail.com>
2019-10-06 16:22:16 +0900

    Worked Dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,31 +12,32 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+
+  String _allowsVOIP;
+  String _carrierName;
+  String _isoCountryCode;
+  String _mobileCountryCode;
+  String _mobileNetworkCode;
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    getUsimInfo();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await SimInfo.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
+  Future<String> getUsimInfo() async {
+    String allowsVOIP = await SimInfo.AllowsVOIP;
+    String carrierName = await SimInfo.AllowsVOIP;
+    String isoCountryCode = await SimInfo.AllowsVOIP;
+    String mobileCountryCode = await SimInfo.AllowsVOIP;
+    String mobileNetworkCode = await SimInfo.AllowsVOIP;
 
     setState(() {
-      _platformVersion = platformVersion;
+      _allowsVOIP = allowsVOIP;
+      _carrierName = carrierName;
+      _isoCountryCode = isoCountryCode;
+      _mobileCountryCode = mobileCountryCode;
+      _mobileNetworkCode = mobileNetworkCode;
     });
   }
 
@@ -45,10 +46,18 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('USIM Infomation Plugin'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Column(
+            children: <Widget>[
+              Text('VOIP : $_allowsVOIP '),
+              Text('CarrierName : $_carrierName'),
+              Text('ISO CountryCode : $_isoCountryCode'),
+              Text('Mobile CountryCode : $_mobileCountryCode'),
+              Text('Mobile NetworkCode : $_mobileNetworkCode'),
+            ],
+          )
         ),
       ),
     );
--- lib/sim_info.dart
+++ lib/sim_info.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 
@@ -6,8 +7,78 @@ class SimInfo {
   static const MethodChannel _channel =
       const MethodChannel('sim_info');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<String> get AllowsVOIP async {
+    bool value;
+    try {
+      if (Platform.isIOS) {
+        value = await _channel.invokeMethod('allowsVOIP');
+      }
+      else if (Platform.isAndroid) {
+        value = await _channel.invokeMethod('allowsVOIP');
+      }
+      return value.toString();
+    } on PlatformException catch(e) {
+      return e.code;
+    }
+  }
+
+  static Future<String> get getCarrierName async {
+    String value;
+    try {
+      if (Platform.isIOS) {
+        value = await _channel.invokeMethod('carrierName');
+      }
+      else if (Platform.isAndroid) {
+        value = await _channel.invokeMethod('carrierName');
+      }
+      return value;
+    } on PlatformException catch(e) {
+      return e.code;
+    }
+  }
+
+  static Future<String> get getIsoCountryCode async {
+    String value;
+    try {
+      if (Platform.isIOS) {
+        value = await _channel.invokeMethod('isoCountryCode');
+      }
+      else if (Platform.isAndroid) {
+        value = await _channel.invokeMethod('isoCountryCode');
+      }
+      return value;
+    } on PlatformException catch(e) {
+      return e.code;
+    }
+  }
+
+  static Future<String> get getMobileCountryCode async {
+    String value;
+    try {
+      if (Platform.isIOS) {
+        value = await _channel.invokeMethod('mobileCountryCode');
+      }
+      else if (Platform.isAndroid) {
+        value = await _channel.invokeMethod('mobileCountryCode');
+      }
+      return value;
+    } on PlatformException catch(e) {
+      return e.code;
+    }
+  }
+
+  static Future<String> get getMobileNetworkCode async {
+    String value;
+    try {
+      if (Platform.isIOS) {
+        value = await _channel.invokeMethod('mobileNetworkCode');
+      }
+      else if (Platform.isAndroid) {
+        value = await _channel.invokeMethod('mobileNetworkCode');
+      }
+      return value;
+    } on PlatformException catch(e) {
+      return e.code;
+    }
   }
 }
