0877ec7afa691758135e432f683635404be5992c
eungikimme <eungikim.me@gmail.com>
2019-10-15 22:46:38 +0900

    Delete platform branch

--- lib/sim_info.dart
+++ lib/sim_info.dart
@@ -8,78 +8,24 @@ class SimInfo {
       const MethodChannel('flutter.moum.sim_info');
 
   static Future<String> get getAllowsVOIP async {
-    bool value;
-    try {
-      if (Platform.isIOS) {
-        value = await _channel.invokeMethod('allowsVOIP');
-      }
-      else if (Platform.isAndroid) {
-        value = await _channel.invokeMethod('allowsVOIP');
-      }
-      return value.toString();
-    } on PlatformException catch(e) {
-      return e.code;
-    }
+    bool value =  await _channel.invokeMethod('allowsVOIP');
+    return value.toString();
   }
 
   static Future<String> get getCarrierName async {
-    String value;
-    try {
-      if (Platform.isIOS) {
-        value = await _channel.invokeMethod('carrierName');
-      }
-      else if (Platform.isAndroid) {
-        value = await _channel.invokeMethod('carrierName');
-      }
-      return value;
-    } on PlatformException catch(e) {
-      return e.code;
-    }
+    return await _channel.invokeMethod('carrierName');
   }
 
   static Future<String> get getIsoCountryCode async {
-    String value;
-    try {
-      if (Platform.isIOS) {
-        value = await _channel.invokeMethod('isoCountryCode');
-      }
-      else if (Platform.isAndroid) {
-        value = await _channel.invokeMethod('isoCountryCode');
-      }
-      return value;
-    } on PlatformException catch(e) {
-      return e.code;
-    }
+    return await _channel.invokeMethod('isoCountryCode');
   }
 
   static Future<String> get getMobileCountryCode async {
-    String value;
-    try {
-      if (Platform.isIOS) {
-        value = await _channel.invokeMethod('mobileCountryCode');
-      }
-      else if (Platform.isAndroid) {
-        value = await _channel.invokeMethod('mobileCountryCode');
-      }
-      return value;
-    } on PlatformException catch(e) {
-      return e.code;
-    }
+    return await _channel.invokeMethod('mobileCountryCode');
   }
 
   static Future<String> get getMobileNetworkCode async {
-    String value;
-    try {
-      if (Platform.isIOS) {
-        value = await _channel.invokeMethod('mobileNetworkCode');
-      }
-      else if (Platform.isAndroid) {
-        value = await _channel.invokeMethod('mobileNetworkCode');
-      }
-      return value;
-    } on PlatformException catch(e) {
-      return e.code;
-    }
+    return await _channel.invokeMethod('mobileNetworkCode');
   }
 
 }
