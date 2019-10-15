769b0b7f862003cbf3437c196d120be88abc49d0
eungikimme <eungikim.me@gmail.com>
2019-10-15 22:48:11 +0900

    Make test
    Fix the method channel name and test five sim information methods

--- test/sim_info_test.dart
+++ test/sim_info_test.dart
@@ -3,11 +3,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:sim_info/sim_info.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('sim_info');
+  const MethodChannel channel = MethodChannel('flutter.moum.sim_info');
 
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+      if (methodCall.method == 'allowsVOIP') return true;
+      if (methodCall.method == 'carrierName') return 'KT';
+      if (methodCall.method == 'isoCountryCode') return 'kr';
+      if (methodCall.method == 'mobileCountryCode') return '450';
+      if (methodCall.method == 'mobileNetworkCode') return '05';
+      return '';
     });
   });
 
@@ -15,7 +20,11 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await SimInfo.platformVersion, '42');
+  test('get 5 sim info method', () async {
+    expect(await SimInfo.getAllowsVOIP, 'true');
+    expect(await SimInfo.getCarrierName, 'KT');
+    expect(await SimInfo.getIsoCountryCode, 'kr');
+    expect(await SimInfo.getMobileCountryCode, '450');
+    expect(await SimInfo.getMobileNetworkCode, '05');
   });
 }
